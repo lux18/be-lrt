f044484829c8898d68cef1fdafc0ab9a4ac34ad1
g37dev <g37dev@gmail.com>
2025-01-10 06:45:33 +0700

    first commit

--- config/database.js
+++ config/database.js
@@ -1,14 +1,15 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-// const db = new Sequelize("lrtdb", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-// });
 
-const db = new Sequelize("defaultdb", "doadmin", process.env.DB_PASS, {
-  host: "db-mysql-nyc3-83512-do-user-15463270-0.j.db.ondigitalocean.com",
-  dialect: "mysql",
-  port: "25060",
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  },
+);
 
 module.exports = db;
