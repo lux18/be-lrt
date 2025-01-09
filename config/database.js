4f74009fd00ed903399ba63fbb0a9eb8cab25d53
g37dev <g37dev@gmail.com>
2025-01-10 06:54:41 +0700

    update config

--- config/database.js
+++ config/database.js
@@ -1,15 +1,14 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
+// const db = new Sequelize("lrtdb", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
-  },
-);
+const db = new Sequelize("defaultdb", "doadmin", "AVNS_p19xy2Ayg4klMaUjV6a", {
+  host: "db-mysql-nyc3-83512-do-user-15463270-0.j.db.ondigitalocean.com",
+  dialect: "mysql",
+  port: "25060",
+});
 
 module.exports = db;
