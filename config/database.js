acb5280c6ce0992c56d1df583b9f986d255e60a6
lux18 <iwaldiputra@gmail.com>
2025-01-10 09:29:11 +0700

    first commit

--- config/database.js
+++ config/database.js
@@ -9,6 +9,7 @@ const db = new Sequelize("defaultdb", "doadmin", "AVNS_p19xy2Ayg4klMaUjV6a", {
   host: "db-mysql-nyc3-83512-do-user-15463270-0.j.db.ondigitalocean.com",
   dialect: "mysql",
   port: "25060",
+  dialectModule: require("mysql2"),
 });
 
 module.exports = db;
